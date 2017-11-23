88f0e9137dfc24098c9edbe6e26ccb7dc7500847
malossep <malossep@Debian9-TP.esisar.inpg.fr>
2017-11-23 16:24:07 +0100

    ajout des infos pr le serveur

--- tmp/Connect.java
+++ tmp/Connect.java
@@ -1,5 +1,6 @@
 import java.sql.*;
 
+//Download ojdbc6.jar then Build Path -> Libraries -> Add External JAR
 public class Connect {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 	String server = "tp-oracle.esisar.grenoble-inp.fr";
@@ -11,41 +12,40 @@ public class Connect {
 		
     Statement stmt = connection.createStatement();
     
-    //Creer une nouvelle table
-    /*String sql = "CREATE TABLE Client" +
+    /////////Creer une nouvelle table
+    /*String sql_create = "CREATE TABLE Client" +
             "(adresseMail VARCHAR(255), " +
             " nom VARCHAR(255), " + 
             " prénom VARCHAR(255), " + 
             " password VARCHAR(255), " + 
             " PRIMARY KEY ( adresseMail ))";
-    stmt.executeUpdate(sql);
+    stmt.executeUpdate(sql_create);
     System.out.println("Table créée"); 
     /*
     
-    //Supprimer la table dans la BDD
+    //////////Supprimer la table dans la BDD
     /*String sql_delete_tab =  "DROP TABLE Client";
     ResultSet rs = stmt.executeQuery(sql_delete_tab);
     System.out.println("Table supprimée");
     */
 	
-    //Ajouter élément
+    //////////Ajouter élément
     /*String sql_element = "INSERT INTO Client " +
             "VALUES ('daz@hdiz', 'pauline', 'malosse', 'xx')";
     stmt.executeUpdate(sql_element);
     System.out.println("Element créé"); 
     */
 
-    //supprimer un élement
+    /////////supprimer un élement
     /*String sql_delete = "DELETE FROM Client " +
             "WHERE adresseMail = 'daz@hdiz'";
     stmt.executeUpdate(sql_delete);
     */
     
-    //affiche la table
+    /////////affiche la table
     String sql_aff = "SELECT adresseMail, nom, prénom, password FROM Client ";
     ResultSet rs = stmt.executeQuery(sql_aff);
-    
-    //STEP 5: Extract data from result set
+
     while(rs.next()){
        //Retrieve by column name
        String id  = rs.getString("adresseMail");
