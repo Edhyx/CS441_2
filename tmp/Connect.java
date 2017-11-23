69dc49c579f1489d5178e102d0c725a808be8001
malossep <malossep@Debian9-TP.esisar.inpg.fr>
2017-11-23 16:06:16 +0100

    mise a jour oracle

--- tmp/Connect.java
+++ tmp/Connect.java
@@ -4,10 +4,63 @@ public class Connect {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 	String server = "tp-oracle.esisar.grenoble-inp.fr";
 	String driver = "oracle.jdbc.driver.OracleDriver";
-	String url = "jdbc:oracle:thin:@://"+server+":1512:xe";
+	String url = "jdbc:oracle:thin:@//"+server+"/xe";
 	String login = "malossep";
 	Class.forName(driver);
 	Connection connection = DriverManager.getConnection(url,login,login);
-	System.out.println("connection successsfully");
+		
+    Statement stmt = connection.createStatement();
+    
+    //Creer une nouvelle table
+    /*String sql = "CREATE TABLE Client" +
+            "(adresseMail VARCHAR(255), " +
+            " nom VARCHAR(255), " + 
+            " prénom VARCHAR(255), " + 
+            " password VARCHAR(255), " + 
+            " PRIMARY KEY ( adresseMail ))";
+    stmt.executeUpdate(sql);
+    System.out.println("Table créée"); 
+    /*
+    
+    //Supprimer la table dans la BDD
+    /*String sql_delete_tab =  "DROP TABLE Client";
+    ResultSet rs = stmt.executeQuery(sql_delete_tab);
+    System.out.println("Table supprimée");
+    */
+	
+    //Ajouter élément
+    /*String sql_element = "INSERT INTO Client " +
+            "VALUES ('daz@hdiz', 'pauline', 'malosse', 'xx')";
+    stmt.executeUpdate(sql_element);
+    System.out.println("Element créé"); 
+    */
+
+    //supprimer un élement
+    /*String sql_delete = "DELETE FROM Client " +
+            "WHERE adresseMail = 'daz@hdiz'";
+    stmt.executeUpdate(sql_delete);
+    */
+    
+    //affiche la table
+    String sql_aff = "SELECT adresseMail, nom, prénom, password FROM Client ";
+    ResultSet rs = stmt.executeQuery(sql_aff);
+    
+    //STEP 5: Extract data from result set
+    while(rs.next()){
+       //Retrieve by column name
+       String id  = rs.getString("adresseMail");
+       String nom = rs.getString("nom");
+       String prénom = rs.getString("prénom");
+       String password = rs.getString("password");
+
+       //Display values
+       System.out.print("adresseMail: " + id);
+       System.out.print(", nom: " + nom);
+       System.out.print(", prénom: " + prénom);
+       System.out.println(", password: " + password);
+    }
+    rs.close();
+    stmt.close();
+    connection.close();
 	}	
 }
