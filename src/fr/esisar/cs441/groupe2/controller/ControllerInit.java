30e148caab0ec09a3bd91271a6ae81d402ba9f30
polettir <polettir@Debian9-TP.esisar.inpg.fr>
2017-11-29 14:40:33 +0100

    ControllerInit fait

--- src/fr/esisar/cs441/groupe2/controller/ControllerInit.java
+++ src/fr/esisar/cs441/groupe2/controller/ControllerInit.java
@@ -1,15 +1,32 @@
 package fr.esisar.cs441.groupe2.controller;
 
+import java.util.ArrayList;
+
+import fr.esisar.cs441.groupe2.model.Model;
+import fr.esisar.cs441.groupe2.view.View;
+
 public class ControllerInit extends Controller{
 
-	@Override
-	public void notifychangement(String changement) {
-		// TODO Auto-generated method stub
-		
+	private String changement;
+	
+	public ControllerInit(View view, Model model) {
+		this.view = view;
+		this.model = model;
+	}
+	
+	public void notifyChangement(String changement) {
+		this.changement=changement;		
 	}
 	
 	public ControllerInit(){
 		
-	}
+		ArrayList<String> vide = new ArrayList<String>();
+		
+		switch(this.changement){
+			case "1": view.displayNewClient();
+			case "2": view.displayConnection(vide);
+			case "9": view.displayEnd(null);
+			}
+		}
 
 }
