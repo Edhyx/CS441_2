29a73a83af777e3d7072ab0d9537afa5eafeb9f8
polettir <polettir@Debian9-TP.esisar.inpg.fr>
2017-11-29 14:43:14 +0100

    ControllerInit fait

--- src/fr/esisar/cs441/groupe2/controller/Controller.java
+++ src/fr/esisar/cs441/groupe2/controller/Controller.java
@@ -3,9 +3,10 @@ package fr.esisar.cs441.groupe2.controller;
 import fr.esisar.cs441.groupe2.model.Model;
 import fr.esisar.cs441.groupe2.view.View;
 
-public abstract class Controller {
+public class Controller {
 	protected View view;
 	protected Model model;
 	
-	public abstract void notifyChangement(String changement);
+	public void notifyChangement(String changement) {
+	}
 }
