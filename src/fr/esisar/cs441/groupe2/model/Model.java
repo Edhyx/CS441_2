a80480bd4043e5bd9fc49157cee497b063a9486c
benmahmm <benmahmm@Debian9-TP.esisar.inpg.fr>
2017-12-01 11:19:25 +0100

    Model

--- src/fr/esisar/cs441/groupe2/model/Model.java
+++ src/fr/esisar/cs441/groupe2/model/Model.java
@@ -1,6 +1,5 @@
 package fr.esisar.cs441.groupe2.model;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -10,27 +9,29 @@ import java.util.concurrent.ThreadLocalRandom;
 
 import fr.esisar.cs441.groupe2.base.entity.*;
 import fr.esisar.cs441.groupe2.base.BDDs.*;
+
 public class Model {
 
 	private String idClient;
 	private Statement stmt;
 
 	public Model() {
-		String driver = "com.mysql.jdbc.Driver";
-		String url = "jdbc:mysql://localhost/test?useSSL=false";
-		String login = "root";
-		String pass = "Pauline02";
+		String server = "tp-oracle.esisar.grenoble-inp.fr";
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@//" + server + "/xe";
+		String login = "malossep";
+		String pass = "malossep";
 		try {
 			Class.forName(driver);
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-		}	
+		}
 		Connection connexion = null;
 		try {
-			connexion = DriverManager.getConnection(url,login,pass);
+			connexion = DriverManager.getConnection(url, login, pass);
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}	
+		}
 
 		Statement stmts = null;
 		try {
@@ -38,36 +39,38 @@ public class Model {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		this.stmt = stmts;
 	}
-	
+
 	public void setClient(String id) {
 		this.idClient = id;
 	}
-	
+
 	public String getClientPassword(String id) {
 		ClientDAO tableClient = new ClientDAO(stmt);
 		Client client = tableClient.getById(id);
-		if (client ==null) {return null;}
-		else
-		return client.getPassword();
-		
+		if (client == null) {
+			return null;
+		} else
+			return client.getPassword();
+
 	}
-	
+
 	private Adresse createAdressF(int idAdresseF, String rue, String codePostal, String ville) {
-		Adresse newAdressF = new Adresse(idAdresseF,rue,codePostal,ville);
+		Adresse newAdressF = new Adresse(idAdresseF, rue, codePostal, ville);
 		AdresseDAO tableAdressF = new AdresseDAO(stmt);
 		try {
 			tableAdressF.add_F(newAdressF);
 		} catch (SQLException e) {
-			
+
 			e.printStackTrace();
 		}
 		return newAdressF;
 	}
+
 	public Adresse createAdressL(int idAdresseL, String rue, String codePostal, String ville) {
-		Adresse newAdressL = new Adresse(idAdresseL,rue,codePostal,ville);
+		Adresse newAdressL = new Adresse(idAdresseL, rue, codePostal, ville);
 		AdresseDAO tableAdressL = new AdresseDAO(stmt);
 		try {
 			tableAdressL.add_F(newAdressL);
@@ -76,8 +79,10 @@ public class Model {
 		}
 		return newAdressL;
 	}
+
 	public boolean createClient(String adresseMail, String nom, String prenom, String password) {
-		Client newClient = new Client(adresseMail, nom, prenom, password, createAdressF(0, null, null, null), createAdressL(0, null, null, null));
+		Client newClient = new Client(adresseMail, nom, prenom, password, createAdressF(0, null, null, null),
+				createAdressL(0, null, null, null));
 		ClientDAO tableClient = new ClientDAO(stmt);
 		try {
 			tableClient.add(newClient);
@@ -86,34 +91,52 @@ public class Model {
 		}
 		return true;
 	}
-	
-	public boolean addFile(String appareilPhoto, String objectif, String distance, String sensibilte, String ouverture, String vitesse) {
-		
-		String url = new String(""+ThreadLocalRandom.current().nextInt(0, 1000 + 1));
-		
-		// completer
+
+	public boolean addFile(String idClient, String appareilPhoto, String objectif, int distance, int sensibilte,
+			int ouverture, int vitesse) {
+
+		String url = new String("" + ThreadLocalRandom.current().nextInt(0, 1000 + 1));
+		ClientDAO tableClient = new ClientDAO(stmt);
+		Client client = tableClient.getById(idClient);
+		FichierImage fichierIm = new FichierImage(url, appareilPhoto, objectif, distance, sensibilte, ouverture,
+				vitesse, client);
+		FichierImageDAO tableFichierIm = new FichierImageDAO(stmt);
+		try {
+			tableFichierIm.add(fichierIm);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+
+	public boolean addFolder(String idClient, String titre, String sousTitre) {
+
+		Integer id = new Integer(ThreadLocalRandom.current().nextInt(0, 1000 + 1));
+		ClientDAO tableClient = new ClientDAO(stmt);
+		Client client = tableClient.getById(idClient);
+		Album album = new Album(id, titre, sousTitre, client);
+		AlbumDAO tableAlbum = new AlbumDAO(stmt);
+		try {
+			tableAlbum.add(album);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 		return true;
+
 	}
-	
-	public boolean addFolder( String titre, String sousTitre) {
-		
-		String id = new String(""+ThreadLocalRandom.current().nextInt(0, 1000 + 1));
-		
-		// completer
-		
-		return true;
-		
-	}
-	
+
 	public ArrayList<String> getFolderList() {
+
 		ArrayList<String> returns = new ArrayList<String>();
-		
-		returns.add("blabla");
-		returns.add("poloplop");
-		
+		ArrayList<Album> album = new ArrayList<Album>();
+		AlbumDAO tableAlbum = new AlbumDAO(stmt);
+		album = tableAlbum.getAll();
+
+		for (Album al : album) {
+			returns.add(album.toString());
+		}
 		return returns;
 	}
-	
-	
+
 }
