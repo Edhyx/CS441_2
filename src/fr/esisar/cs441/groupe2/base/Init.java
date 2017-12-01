e2e8d2cecbdf8f48047f10b2296aa0afa2dd4d23
malossep <malossep@Debian9-TP.esisar.inpg.fr>
2017-12-01 09:45:39 +0100

    modif

--- src/fr/esisar/cs441/groupe2/base/Init.java
+++ src/fr/esisar/cs441/groupe2/base/Init.java
@@ -10,10 +10,11 @@ public class Init {
 	 * @param args
 	 */
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		String driver = "com.mysql.jdbc.Driver";
-		String url = "jdbc:mysql://localhost/test?useSSL=false";
-		String login = "root";
-		String pass = "Pauline02";
+		String server = "tp-oracle.esisar.grenoble-inp.fr";
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@//"+server+"/xe";
+		String login = "malossep";
+		String pass ="malossep";
 			Class.forName(driver);	
 			Connection connexion = DriverManager.getConnection(url,login,pass);	
 			System.out.println("Connection OK!"); 
