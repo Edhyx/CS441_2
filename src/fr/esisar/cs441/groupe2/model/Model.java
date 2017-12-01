46464eca6e2c3596ed365ce436488a11565a9e6e
benmahmm <benmahmm@Debian9-TP.esisar.inpg.fr>
2017-12-01 09:20:50 +0100

    Model

--- src/fr/esisar/cs441/groupe2/model/Model.java
+++ src/fr/esisar/cs441/groupe2/model/Model.java
@@ -1,13 +1,19 @@
 package fr.esisar.cs441.groupe2.model;
 
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
-import fr.esisar.cs441.groupe2.base.entity.Client;
-
+import fr.esisar.cs441.groupe2.base.entity.*;
+import fr.esisar.cs441.groupe2.base.BDDs.*;
 public class Model {
 
 	private String idClient;
+	private Statement stmt;
+
+	public Model(Statement stmt) {
+		this.stmt = stmt;
+	}
 	
 	public void setClient(String id) {
 		this.idClient = id;
@@ -20,8 +26,20 @@ public class Model {
 		return null;
 	}
 	
-	public boolean createClient(String adresse, String nom, String prenom, String password) {
-		Client newClient = new Client(adresse, nom, prenom, password, null, null);
+	private Adresse createAdressF(int idAdresseF, String rue, String codePostal, String ville) {
+		Adresse newAdressF = new Adresse(idAdresseF,rue,codePostal,ville);
+		AdresseDAO tableAdressF = new AdresseDAO(stmt);
+		tableAdressF.add_F(newAdressF);
+		return newAdressF;
+	}
+	public Adresse createAdressL(int idAdresseL, String rue, String codePostal, String ville) {
+		Adresse newAdressL = new Adresse(idAdresseL,rue,codePostal,ville);
+		AdresseDAO tableAdressL = new AdresseDAO(stmt);
+		tableAdressL.add_F(newAdressL);
+		return newAdressL;
+	}
+	public boolean createClient(String adresseMail, String nom, String prenom, String password) {
+		Client newClient = new Client(adresseMail, nom, prenom, password, createAdressF, adresseL);
 		// completer
 		return true;
 	}
