8532dd60af52de67d3c4b1ba601838d3234c78aa
Mathieu <thalfeust@gmail.com>
2017-12-01 09:50:14 +0100

    simulation

--- src/fr/esisar/cs441/groupe2/model/Model.java
+++ src/fr/esisar/cs441/groupe2/model/Model.java
@@ -1,88 +1,28 @@
 package fr.esisar.cs441.groupe2.model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
-import fr.esisar.cs441.groupe2.base.entity.*;
-import fr.esisar.cs441.groupe2.base.BDDs.*;
+import fr.esisar.cs441.groupe2.base.entity.Client;
+
 public class Model {
 
 	private String idClient;
-	private Statement stmt;
-
-	public Model() {
-		String driver = "com.mysql.jdbc.Driver";
-		String url = "jdbc:mysql://localhost/test?useSSL=false";
-		String login = "root";
-		String pass = "Pauline02";
-		try {
-			Class.forName(driver);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}	
-		Connection connexion = null;
-		try {
-			connexion = DriverManager.getConnection(url,login,pass);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}	
-
-		Statement stmts = null;
-		try {
-			stmts = connexion.createStatement();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		this.stmt = stmts;
-	}
 	
 	public void setClient(String id) {
 		this.idClient = id;
 	}
 	
 	public String getClientPassword(String id) {
-		ClientDAO tableClient = new ClientDAO(stmt);
-		Client client = tableClient.getById(id);
-		if (client ==null) {return null;}
-		else
-		return client.getPassword();
-		
+		if(id.equals("plop")) {
+			return "123456";
+		}
+		return null;
 	}
 	
-	private Adresse createAdressF(int idAdresseF, String rue, String codePostal, String ville) {
-		Adresse newAdressF = new Adresse(idAdresseF,rue,codePostal,ville);
-		AdresseDAO tableAdressF = new AdresseDAO(stmt);
-		try {
-			tableAdressF.add_F(newAdressF);
-		} catch (SQLException e) {
-			
-			e.printStackTrace();
-		}
-		return newAdressF;
-	}
-	public Adresse createAdressL(int idAdresseL, String rue, String codePostal, String ville) {
-		Adresse newAdressL = new Adresse(idAdresseL,rue,codePostal,ville);
-		AdresseDAO tableAdressL = new AdresseDAO(stmt);
-		try {
-			tableAdressL.add_F(newAdressL);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return newAdressL;
-	}
-	public boolean createClient(String adresseMail, String nom, String prenom, String password) {
-		Client newClient = new Client(adresseMail, nom, prenom, password, createAdressF(0, null, null, null), createAdressL(0, null, null, null));
-		ClientDAO tableClient = new ClientDAO(stmt);
-		try {
-			tableClient.add(newClient);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+	public boolean createClient(String adresse, String nom, String prenom, String password) {
+		Client newClient = new Client(adresse, nom, prenom, password, null, null);
+		// completer
 		return true;
 	}
 	
